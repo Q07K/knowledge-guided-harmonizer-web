a97f4d1fc5bfd3fea4fe99cdc1270b3508b20805
gyuhyeong <kgh0730@gmail.com>
2025-09-27 10:05:03 +0900

    Feat: main.js에서 스타일 시트 경로 수정 및 _base.scss 추가
    - '@/assets/styles.css' 경로로 수정
    - '@/assets/_base.scss' 파일 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/styles.css'
+
+import '@/assets/styles.css'
+import '@/assets/_base.scss'
 
 // Create and mount the app
 const app = createApp(App)
